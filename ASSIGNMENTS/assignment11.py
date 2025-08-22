51ca1c090545ef241e26400b2f86ee02edddb26e
kamsi <kamsimbah09@gmail.com>
2025-08-22 14:42:53 +0100

    Python Assignment

--- ASSIGNMENTS/assignment11.py
+++ ASSIGNMENTS/assignment11.py
@@ -2,9 +2,9 @@
 This program demonstrates a simple yes or no
 It keeps asking the user if he/she wants to continue,they will choose Y to continue but N to end the program.
 """
+
 while True:
     num = int(input("Enter a number:"))
-
     print("You entered:",num)
 
     choice = input("Do you want to continue?(Y/N):")
@@ -13,4 +13,5 @@ while True:
         choice = input("Invalid choice")
 
     if choice == 'N':
-        print()    
+        print("You are done") 
+        break   
